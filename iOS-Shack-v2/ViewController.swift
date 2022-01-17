d57160fbe92262f0c877bb7e534aadf01df73a32
Igor Stojanovic <stojanovicigi@gmail.com>
2022-01-17 10:22:18 +0100

    Comment out aws credentials from ViewController

--- iOS-Shack-v2/ViewController.swift
+++ iOS-Shack-v2/ViewController.swift
@@ -33,8 +33,8 @@ class ViewController: UIViewController, MenuControllerDelegate {
         }
         
         
-        let aws_access_key = "***REMOVED***"
-        let aws_secret_access_key = "***REMOVED***"
+//        let aws_access_key = "***REMOVED***"
+//        let aws_secret_access_key = "***REMOVED***"
         
         menu.delegate = self
 
